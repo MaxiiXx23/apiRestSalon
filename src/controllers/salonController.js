8e912a1423356abd74076f025b5ae312c2ec7193
MaxiiXx23 <max.232017@gmail.com>
2022-05-15 13:20:06 -0300

    Criação da rota profile

--- src/controllers/salonController.js
+++ src/controllers/salonController.js
@@ -1,8 +1,34 @@
-import multer from 'multer';
-import profileSalonConfig from '../config/profileSalonConfig';
 import { Salon } from '../models/Salon';
 
-const profileUpload = multer(profileSalonConfig).single('profile');
+
+async function profile(req, res) {
+    try{
+        const { idSalon } = req.params;
+        if(!idSalon) {
+            return res.status(400).json({
+                errros:['Parameter idSalon not found.']
+            })
+        }
+        const salon = await Salon.findByPk(idSalon,{
+            attributes:['id', 'nome', 'fotoPerfil', 'banner', 'avaliacao']
+        });
+        if(!salon){
+            return res.status(400).json({
+                errors:['Salon not exists.']
+            })
+        }
+
+        return res.status(200).json(salon)
+    }catch(e) {
+        return res.status(400).json({
+            errors: e.errors.map( error => error.massage)
+        })
+    }
+}
+
+
+
+
 
 async function update(req,res) {
     try{
@@ -30,4 +56,4 @@ async function update(req,res) {
 }
 
 
-export {update};
+export {profile, update};
--- src/routes/salonRoutes.js
+++ src/routes/salonRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { update } from "../controllers/salonController";
+import { profile, update } from "../controllers/salonController";
 import { tokenMiddleware } from '../middlewares/tokenMiddleware';
 
 const router = new Router();
 
+router.get('/profile/:idSalon', tokenMiddleware, profile);
 router.put ('/updateSalon/:idSalon', tokenMiddleware, update);
 
 
